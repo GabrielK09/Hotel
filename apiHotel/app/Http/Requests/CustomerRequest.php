285e4a3a32a4ae5cf6092293f2e762f5f230d0a6
Gabriel Kochem <gabikochem55@gmail.com>
2025-01-29 17:03:56 -0300

    A

--- apiHotel/app/Http/Requests/CustomerRequest.php
+++ apiHotel/app/Http/Requests/CustomerRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class CustomerRequest extends FormRequest
 {
@@ -30,5 +32,37 @@ class CustomerRequest extends FormRequest
             'address' => [$required, 'string', 'max:120']
 
         ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O campo nome é obrigátorio',
+            'name.max' => 'O campo nome passou do seu limite: :max caracteres',
+            'name.string' => 'O campo nome deve ser texto válido',
+
+            'cnpj.required' => 'O campo CNPJ é obrigátorio',
+            'cnpj.max' => 'O campo CNPJ passou do seu limite: :max caracteres',
+            'cnpj.string' => 'O campo CNPJ deve ser texto válido',
+
+            'cpf.required' => 'O campo CPF é obrigátorio',
+            'cpf.max' => 'O campo CPF passou do seu limite: :max caracteres',
+            'cpf.string' => 'O campo CPF deve ser texto válido',
+
+            'address.required' => 'O campo endereço é obrigátorio',
+            'address.max' => 'O campo endereço passou do seu limite: :max caracteres',
+
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Erro de validação ( Customer )',
+                'errors' => $validator->errors(),
+
+            ], 422)
+        );
     }
 }
